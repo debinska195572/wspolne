cbe2f865f012989fb02dad15f9e77c00aea5b398
debinska19572 <debinska4@gmail.com>
2017-03-08 08:53:47 +0100

    dodanie funkcji
    dodanie funkcji

--- 42/src/main/java/test/App.java
+++ 42/src/main/java/test/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+    	
+    	public void liczby(){
+    		for(int i=0;i<10;i++)
+    			System.out.println(i+" ");
+       	}
+    	
         System.out.println( "Hello World!" );
     }
 }
