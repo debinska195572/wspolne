8bcec04ff00a0ed52aedae3f17c4cb139b71ab05
golec195594 <agatagc@op.pl>
2017-03-08 08:57:21 +0100

    f

--- 42/src/main/java/test/App.java
+++ 42/src/main/java/test/App.java
@@ -4,6 +4,7 @@ package test;
  * Hello world!
  *
  */
+<<<<<<< HEAD
 
 
 public void funAM()
@@ -16,6 +17,10 @@ public void funAM()
 
 public class App 
 {
+=======
+public class App 
+{
+>>>>>>> parent of 663d9bd... funkcja imiona
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
