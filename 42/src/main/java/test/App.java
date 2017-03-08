5982bfca34ce2ac00ebe86acc66738f218706705
masternak195656 <195656@edu.p.lodz.pl>
2017-03-08 09:23:43 +0100

    WM
    WM

--- 42/src/main/java/test/App.java
+++ 42/src/main/java/test/App.java
@@ -31,21 +31,19 @@ public class App
 
     public static void main( String[] args )
     {
-<<<<<<< HEAD
     	
     	public void liczby(){
     		for(int i=0;i<10;i++)
     			System.out.println(i+" ");
        	}
     	
-=======
+
     	   	
     	
     	public void wypisz imiona()
     	{
     		 System.out.println( "Agata, Ola1, Ola2, Wojtek, Marcik" );
     	}
->>>>>>> 7e683afe03fbed09c534f5899d3ddcfdf449ebc5
         System.out.println( "Hello World!" );
     }
 	
