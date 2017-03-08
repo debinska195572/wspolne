663d9bd84a26d3b7162f4977bfdcbde3e1519cd1
golec195594 <agatagc@op.pl>
2017-03-08 08:50:14 +0100

    funkcja imiona

--- 42/src/main/java/test/App.java
+++ 42/src/main/java/test/App.java
@@ -4,10 +4,23 @@ package test;
  * Hello world!
  *
  */
+
+
+
 public class App 
 {
+	
+	
+	
+	
     public static void main( String[] args )
     {
+    	   	
+    	
+    	public void wypisz imiona()
+    	{
+    		 System.out.println( "Agata, Ola1, Ola2, Wojtek, Marcik" );
+    	}
         System.out.println( "Hello World!" );
     }
 }
