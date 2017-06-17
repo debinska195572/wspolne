c1f90b60ce620519997504b283be207f4ba58e2d
Marcin <195603@edu.p.lodz.pl>
2017-06-17 04:14:03 +0200

    Revert "Revert "Conieco dodane""
    This reverts commit d31b566420ecdb7fcbc146c16340a5d35ba89e65.

--- Menu_Manager/src/main/java/calculations/GenerateRecipe.java
+++ Menu_Manager/src/main/java/calculations/GenerateRecipe.java
@@ -5,8 +5,15 @@ package calculations;
 
 
 
+
+import java.util.List;
+
+import javax.swing.JList;
+
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+
 
 import databaseManager.*;
 
@@ -20,43 +27,65 @@ public final class GenerateRecipe {
 	private int forSwitch;
 	private Session session;
 	private Transaction tx;
+	private static List<Recipe> recipes;
 	
-	public GenerateRecipe(User user)
+	
+	public static /*JList<Recipe>*/ void  getLunch(User loggedUser, Session sessionDB)
 	{
-		this.user = user;
-	
-	}
-	
-	public GenerateRecipe(){
+		//List<Recipe> list = (List) sessionDB.createCriteria(Recipe.class).list();
 		
-		
-		
-		
-		session = HibernateUtil.getSessionFactory().openSession();
-		tx = null;
-		try {
-		   tx = session.beginTransaction();
-		   
-		   tryToGetGoodRecipe();
-		   
-		   tx.commit();
-		}
-		catch (Exception e) {
-		   if (tx!=null) tx.rollback();
-		   e.printStackTrace(); 
-		}finally {
-		   session.close();
-		}
-		
-		
-	}
 
-	private void tryToGetGoodRecipe() {
+		// Create CriteriaBuilder
+		//CriteriaBuilder builder = sessionDB.getCriteriaBuilder();
 		
+		//Criteria builder= (Criteria) sessionDB.createCriteria(Recipe.class).list();
+
+		// Create CriteriaQuery
+		//CriteriaQuery<Recipe> criteria = builder.createQuery(Recipe.class);
 		
+		recipes = sessionDB.createQuery("FROM Recipe").list();
+		System.out.println("Ilość: " + recipes.size());
 		
+		//return null;
 		
 	}
+	
+//	public GenerateRecipe(User user)
+//	{
+//		this.user = user;
+//	
+//	}
+//	
+//	public GenerateRecipe(){
+//		
+//		
+//		
+//		
+//		session = HibernateUtil.getSessionFactory().openSession();
+//		tx = null;
+//		try {
+//		   tx = session.beginTransaction();
+//		   
+//		   tryToGetGoodRecipe();
+//		   
+//		   tx.commit();
+//		}
+//		catch (Exception e) {
+//		   if (tx!=null) tx.rollback();
+//		   e.printStackTrace(); 
+//		}finally {
+//		   session.close();
+//		}
+//		
+//		
+//	}
+//
+//	private void tryToGetGoodRecipe() {
+//		
+//		
+//		
+//		
+//	}
 	
 	
 }
