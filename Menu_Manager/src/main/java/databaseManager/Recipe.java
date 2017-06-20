dd01ddc683a8620cdd960957a283de38cdc509dd
Aleksandra <debinska4@gmail.com>
2017-06-20 21:25:31 +0200

    poprawienie lomboka i formatowanie kodu

--- Menu_Manager/src/main/java/databaseManager/Recipe.java
+++ Menu_Manager/src/main/java/databaseManager/Recipe.java
@@ -3,7 +3,6 @@ package databaseManager;
 import java.util.HashSet;
 import java.util.Set;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -14,85 +13,52 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Recipes")
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Recipe {
-
-	private String recipeName;
-	private String recipeType;
-	private User owner;
-	private String content;
-	private Set<RecipeIngredient> ri= new HashSet<RecipeIngredient>();
-
-	public Recipe() {
-		// TODO Auto-generated constructor stub
-	}
-
-	
-
-	public Recipe( String recipeName, String recipeType,User user,  String content) {
-		super();
-		
-		this.recipeName = recipeName;
-		this.recipeType = recipeType;
-		this.owner=user;
-		this.content = content;
-	}
 
 	@Id
 	@Column(name = "Recipe_Name")
-	public String getRecipeName() {
-		return recipeName;
-	}
-
-	public void setRecipeName(String recipeName) {
-		this.recipeName = recipeName;
-	}
-	
+	private String recipeName;
 	@Column(name = "Recipe_Type")
-	public String getRecipeType() {
-		return recipeType;
-	}
-
-	public void setRecipeType(String recipeType) {
-		this.recipeType = recipeType;
-	}
-
+	private String recipeType;
 	@ManyToOne
-	@JoinColumn(name="Owner")
-	public User getOwner() {
-		return owner;
-	}
-
-	public void setOwner(User owner) {
-		this.owner = owner;
-	}
-
+	@JoinColumn(name = "Owner")
+	private User owner;
 	@Column(name = "Content")
-	public String getContent() {
-		return content;
-	}
+	private String content;
+	@OneToMany(mappedBy = "recipe", fetch = FetchType.LAZY)
+	private Set<RecipeIngredient> ri = new HashSet<RecipeIngredient>();
 
-	public void setContent(String content) {
+	public Recipe(String recipeName, String recipeType, User user, String content) {
+		super();
+		this.recipeName = recipeName;
+		this.recipeType = recipeType;
+		this.owner = user;
 		this.content = content;
 	}
 
-	  @OneToMany(mappedBy = "recipe", fetch = FetchType.LAZY)
-	    public Set<RecipeIngredient> getRecipesIngredients() {
-	        return ri;
-	    }
-	 
-	    public void setRecipesIngredients(Set<RecipeIngredient> newRI) {
-	        this.ri = newRI;
-	    }
-	     
-	    public void addRecipeIngredient(RecipeIngredient newRI) {
-	        this.ri.add(newRI);
-	    }   
-	
-	    public void removeRecipeIngredient(RecipeIngredient deletedRI) {
-	        this.ri.remove(deletedRI);
-	    }   
+	public Set<RecipeIngredient> getRecipesIngredients() {
+		return ri;
+	}
+
+	/*public void setRecipesIngredients(Set<RecipeIngredient> newRI) {
+		this.ri = newRI;
+	}*/
+
+	public void addRecipeIngredient(RecipeIngredient newRI) {
+		this.ri.add(newRI);
+	}
+
+	public void removeRecipeIngredient(RecipeIngredient deletedRI) {
+		this.ri.remove(deletedRI);
+	}
 }
--- Menu_Manager/src/main/java/databaseManager/User.java
+++ Menu_Manager/src/main/java/databaseManager/User.java
@@ -8,8 +8,15 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "Users")
+@NoArgsConstructor
+@Setter
+@Getter
 public class User {
 
 	@Id
@@ -31,21 +38,14 @@ public class User {
 	private String diet;
 	@Column(name = "Gender")
 	private String gender;
-	
-	@OneToMany(mappedBy="owner")
+	@OneToMany(mappedBy = "owner")
 	private Set<Recipe> recipes;
-	
-	public User() {
-
-	}
 
 	public User(String login, String password) {
-this.login=login;
-this.password= password;
+		this.login = login;
+		this.password = password;
 	}
-	
-	
-	
+
 	public User(String login, String password, int age, int height, int weight, boolean lactoseTolerance,
 			boolean glutenTolerance, String diet, String gender) {
 		super();
@@ -57,89 +57,6 @@ this.password= password;
 		this.lactoseTolerance = lactoseTolerance;
 		this.glutenTolerance = glutenTolerance;
 		this.diet = diet;
-		this.gender= gender;
-	}
-
-	public String getGender() {
-		return gender;
-	}
-
-	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public void setHeight(int height) {
-		this.height = height;
-	}
-
-	public int getWeight() {
-		return weight;
-	}
-
-	public void setWeight(int weight) {
-		this.weight = weight;
-	}
-
-	public boolean isLactoseTolerance() {
-		return lactoseTolerance;
-	}
-
-	public void setLactoseTolerance(boolean lactoseTolerance) {
-		this.lactoseTolerance = lactoseTolerance;
-	}
-
-	
-
-	public boolean isGlutenTolerance() {
-		return glutenTolerance;
-	}
-
-	public void setGlutenTolerance(boolean glutenTolerance) {
-		this.glutenTolerance = glutenTolerance;
-	}
-
-	public String getDiet() {
-		return diet;
-	}
-
-	public void setDiet(String diet) {
-		this.diet = diet;
-	}
-
-	public Set<Recipe> getRecipes() {
-		return recipes;
-	}
-
-	public void setRecipes(Set<Recipe> recipes) {
-		this.recipes = recipes;
-	}
-
 }
